968100158e640cf14344fbf3d5ae8175ffea1549
FIC-Nati <fastcaserouge19@gmail.com>
2022-03-26 18:22:43 +0300

    afds

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -22,3 +22,18 @@ char *cap_string(char *str)
 				trigger = 1;
 		}
 
+		if (trigger)
+		{
+			if (str[i] > 96 && str[i] < 123)
+			{
+				str[i] -= 32;
+				trigger = 0;
+			}
+			else if (str[i] > 64 && str[i] < 91)
+				trigger = 0;
+			else if (str[i] > 47 && str[i] < 58)
+				trigger = 0;
+		}
+	}
+	return (str);
+}
