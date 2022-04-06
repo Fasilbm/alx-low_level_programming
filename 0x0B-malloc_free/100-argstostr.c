b7b179c87d358a35fe3076c2b687c088558c9070
FIC-Nati <fastcaserouge19@gmail.com>
2022-04-06 20:52:32 +0300

    asddsa

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,45 +1,44 @@
+#include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * argstostr - concatenates all the arguments of your program
- * @ac: argument count
- * @av: argument vector
- * Return: char
- */
-
+ *argstostr - Do somethng
+ * Desc: argstostr function
+ * @ac: int type
+ * @av: int type
+ * Return: Return
+ **/
 char *argstostr(int ac, char **av)
 {
-	char *ar, *str;
-	int i, j, cont;
+int i, j, x = 0, y = 0;
+char *s;
+if (ac == 0 || av == NULL)
+return (NULL);
 
-	if (ac == 0 || av == NULL)
-		return (NULL);
-	for (i = 0; i < ac; i++)
-	{
-		j = 0;
-		while (av[i][j] != '\0')
-		{
-			j++;
-			cont++;
-		}
-		cont++;
-	}
-	cont += 1;
-	ar = malloc(cont * sizeof(char));
-	if (ar == NULL)
-		return (NULL);
-	str = ar;
-	for (i = 0; i < ac; i++)
-	{
-		j = 0;
-		while (av[i][j] != '\0')
-		{
-			*ar = av[i][j];
-			j++;
-			ar++;
-		}
-		*ar = '\n';
-		ar++;
-	}
-	return (str);
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j]; j++)
+
+y++;
+y++;
+
+}
+y++;
+s = malloc(y *sizeof(char));
+if (s == NULL)
+return (NULL);
+
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j]; j++)
+{
+s[x] = av[i][j];
+x++;
+}
+s[x] = '\n';
+x++;
+}
+s[x] = '\0';
+return (s);
 }
