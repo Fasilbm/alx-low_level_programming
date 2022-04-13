fc79539030dda282dfb042d46a35a5cbd212763a
FIC-Nati <fastcaserouge19@gmail.com>
2022-04-13 11:40:27 +0300

    correction

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -37,7 +37,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog->owner = malloc(ol * sizeof(char));
 	if (dog->owner == NULL)
 	{
-free(dog->name);
+                free(dog->name);
 		free(dog);
 		return (NULL);
 	}
